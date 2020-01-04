88eba25f25e1ea436dbe494813525e7ceefff7d0
taktakahashi <takadecem13@gmail.com>
2020-01-04 10:35:23 +0900

    scssを使えるようにする

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import autoPreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -18,7 +19,8 @@ export default {
 		svelte({
 			// enable run-time checks when not in production
 			dev: !production,
-			// we'll extract any component CSS out into
+      preprocess: autoPreprocess(),
+      // we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
 				css.write('public/build/bundle.css');
