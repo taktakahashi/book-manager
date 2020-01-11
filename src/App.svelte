d93f3298d160c29f1a6ee22d1e74ef5592b497f5
taktakahashi <takadecem13@gmail.com>
2020-01-12 03:23:33 +0900

    ナビゲーションバーを作る

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,39 @@
 <script>
-	export let name;
+  let books = [
+    { id: 1, name: 'ゼロから始めるデザインシステム', imgUrl: 'https://i.gyazo.com/a3ddad4c037aca4778628c826a64ce4c.jpg' },
+    { id: 2, name: 'アスキーアート技術読本', imgUrl: 'https://i.gyazo.com/3992e2f40adb6706f9f187fe3c8843d4.jpg' },
+    { id: 3, name: 'ポモドーロ GTD すきま時間予定表で目標達成する本', imgUrl: 'https://i.gyazo.com/0e69af4e7fd229fae9c6b6c44c9bb4d9.png' },
+    { id: 4, name: 'Nuxt Svelte Ionicで作る爆速フロントエンド 3つの世界', imgUrl: 'https://i.gyazo.com/a5db257b7b48210536d387ba9ad5da80.jpg' }
+  ]
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <nav class="nav-tab">
+    <ul class="nav-tab-list">
+      { #each books as book (book.id) }
+        <li class="nav-tab-list-el">
+          <img class="book-thumb" src={ book.imgUrl }>
+        </li>
+      { /each }
+    </ul>
+  </nav>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<style type="text/scss">
+  .nav-tab {
+    .nav-tab-list {
+      list-style-type: none;
+      display: flex;
+      justify-content: center;
+      margin-bottom: 0;
+      .nav-tab-list-el {
+        min-width: 100px;
+        margin: 16px 16px 0;
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+        .book-thumb {
+          height: 100px;
+        }
+      }
+    }
+  }
 </style>
